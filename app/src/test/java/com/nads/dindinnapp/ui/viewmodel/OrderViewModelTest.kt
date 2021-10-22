7d08ca46339e0dd732d16266fb654d92524dae2d
nadee <nadeemsani786@hotmail.com>
2021-10-22 17:14:27 +0530

    Initial

---
+++ app/src/test/java/com/nads/dindinnapp/ui/viewmodel/OrderViewModelTest.kt
@@ -0,0 +1,76 @@
+package com.nads.dindinnapp.ui.viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class OrderViewModelTest {
+
+    @Test
+    fun getLoading() {
+    }
+
+    @Test
+    fun getItems2() {
+    }
+
+    @Test
+    fun getIngredients() {
+    }
+
+    @Test
+    fun getLastSelectedCategory() {
+    }
+
+    @Test
+    fun setLastSelectedCategory() {
+    }
+
+    @Test
+    fun getSelecteditem() {
+    }
+
+    @Test
+    fun getSnackBarEventFire() {
+    }
+
+    @Test
+    fun getSnackBarEvent() {
+    }
+
+    @Test
+    fun getOrderlist() {
+    }
+
+    @Test
+    fun getTickerRx() {
+    }
+
+    @Test
+    fun testGetIngredients() {
+    }
+
+    @Test
+    fun tickerFlow() {
+    }
+
+    @Test
+    fun tickerRx() {
+    }
+
+    @Test
+    fun getOrder() {
+    }
+
+    @Test
+    fun getCategory() {
+    }
+
+    @Test
+    fun countdownTimer() {
+    }
+
+    @Test
+    fun getItemBinding2Modified() {
+    }
+}
